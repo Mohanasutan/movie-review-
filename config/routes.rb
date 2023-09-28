ab672e673394c2d02a50b3f37111f0cf90cb4ed8
Mohanasutan Sakthivel <smsutan2019@gmail.com>
2023-09-28 14:32:01 +0530

    Movie review system app

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+ Rails.application.routes.draw do
+    devise_for :users
+    resources :movies do
+      resources :reviews
+    end
+    root 'movies#index'
+    get '/movies/search', to: 'movies#search', as: 'search_movies'
+  end
+
