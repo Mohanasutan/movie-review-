1632ada2b90deeeafaf52bdedb8f92b07438d1f3
Mohanasutan Sakthivel <smsutan2019@gmail.com>
2023-09-28 18:57:03 +0530

    Movie review system app - added request movies option

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_26_054636) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_28_113855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_26_054636) do
   create_table "movies", force: :cascade do |t|
     t.string "name"
     t.date "release_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.string "movie_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
